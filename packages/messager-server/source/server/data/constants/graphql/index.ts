864dba861e722ba0e9ca9ace1d02e2e7bf84e4ea
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-10-16 20:58:57 +0300

    messager-server > dev: server/data/constants/graphql

--- packages/messager-server/source/server/data/constants/graphql/index.ts
+++ packages/messager-server/source/server/data/constants/graphql/index.ts
@@ -1,5 +1,5 @@
 // #region module
 export const GRAPHQL_FAVICON = '/favicon.ico';
 export const GRAPHQL_TITLE = 'API · messager';
-export const GRAPHQL_ENDPOINT = process.env.MESSAGER_ENDPOINT_GRAPHQL || '/messager';
+export const GRAPHQL_ENDPOINT = process.env.MESSAGER_ENDPOINT_GRAPHQL || '/graphql';
 // #endregion module
