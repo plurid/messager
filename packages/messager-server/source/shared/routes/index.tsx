4c0d32a9c410422b2f961ca9a188eb536945bea6
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-10-26 20:26:01 +0300

    messager-server > dev: shared/routes

--- packages/messager-server/source/shared/routes/index.tsx
+++ packages/messager-server/source/shared/routes/index.tsx
@@ -58,26 +58,11 @@ const notFoundRoute: PluridReactRoute = {
             title="not found · messager"
         />
     ),
-    spaces: [
-        {
-            value: 'default',
-            universes: [
-                {
-                    value: 'default',
-                    clusters: [
-                        {
-                            value: 'default',
-                            planes: [
-                                {
-                                    value: '/',
-                                    component: NotFoundPlane,
-                                },
-                            ],
-                        },
-                    ],
-                },
-            ],
-        },
+    planes: [
+        [ '/not-found', NotFoundPlane ],
+    ],
+    view: [
+        '/not-found',
     ],
 };
 
