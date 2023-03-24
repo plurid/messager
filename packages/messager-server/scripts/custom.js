f5e30895015031e48295b0d8fa44852bbf2828ac
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2023-03-24 14:20:30 +0200

    messager-server > setup: package

--- packages/messager-server/scripts/custom.js
+++ packages/messager-server/scripts/custom.js
@@ -4,18 +4,21 @@
 const esModules = [
     // add dependencies to be bundled as ES modules
     // string or (dependency: string): string
+    'bson',
 ];
 
 
 const externals = [
     // add dependencies to be left as external to the server
     // string or (dependency: string): string
+    'bson',
 ];
 
 
 const environment = {
     // add environment variables replacements for frontend, e.g.
     // 'process.env.ENV_NAME': JSON.stringify(process.env.ENV_NAME || ''),
+    'process.env.MESSAGER_ENDPOINT_GRAPHQL': JSON.stringify(process.env.MESSAGER_ENDPOINT_GRAPHQL || ''),
 };
 
 
